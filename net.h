8dfb8a1e7369a77f4379b1b69b067a1b42adbfc8
ZacharyForman <au.zachary.forman@gmail.com>
2015-02-08 11:23:27 +1100

    Branched off a proper net library

---
+++ net.h
@@ -0,0 +1,15 @@
+#ifndef NET_H
+#define NET_H
+
+#if __cplusplus < 201100L
+#error "Requires C++11 features"
+#else
+
+#include "net_error.h"
+#include "socket.h"
+#include "serversocket.h"
+#include "listen_and_serve.h"
+
+#endif // __cplusplus < 201100L
+
+#endif // NET_H
