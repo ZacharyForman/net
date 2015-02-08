6d292ea44641c5ff29c3624fe41d941ab84d76bd
ZacharyForman <au.zachary.forman@gmail.com>
2015-02-08 22:09:10 +1100

    State machines make my life easy!

---
+++ http_utils.cc
@@ -0,0 +1,79 @@
+#if __cplusplus >= 201100L
+
+#include "http_utils.h"
+#include <map>
+#include <string>
+
+namespace net {
+
+namespace {
+
+enum State {
+  q1, q2, q3, q4, q5
+};
+
+}
+
+::std::map<::std::string, ::std::string> read_headers(const char *str, const char **end)
+{
+  ::std::map<::std::string, ::std::string> ret;
+  State st = q1;
+  ::std::string key, val;
+  char *start;
+  for (;*str;) {
+    switch (st) {
+      case q1: {
+        if (*str == ':') {
+          st = q2;
+        } else if (*str == '\r' && key.length() == 0) {
+          st = q4;
+        } else if (*str == '\r' || *str == '\n') {
+          goto err;
+        } else {
+          key += *str;
+        }
+        break;
+      }
+      case q2: {
+        if (*str == '\r') {
+          ret[key] = val;
+          key.clear();
+          val.clear();
+          st = q3;
+        } else {
+          val += *str;
+        }
+        break;
+      }
+      case q3: {
+        if (*str == '\n') {
+          st = q1;
+        } else {
+          goto err;
+        }
+        break;
+      }
+      case q4: {
+        if (*str == '\n') {
+          st = q5;
+        } else {
+          goto err;
+        }
+        break;
+      }
+      case q5: {
+        *end = str;
+        return ret;
+      }
+    }
+    str++;
+  }
+err:
+  ret.clear();
+  *end = nullptr;
+  return ret;
+}
+
+} // net
+
+#endif
--- http_utils.h
+++ http_utils.h
@@ -1,14 +1,20 @@
-#ifndef HTTP_STATUS_H
-#define HTTP_STATUS_H
+#ifndef HTTP_UTILS_H
+#define HTTP_UTILS_H
 
 #if __cplusplus < 201100L
 #error "Requires C++11 features"
 #else
 
+#include <map>
+#include <string>
+
 namespace net {
+
+::std::map<::std::string, ::std::string>
+read_headers(const char *str, const char **end);
 
 } // net
 
 #endif // __cplusplus >= 201100L
 
-#endif // HTTP_STATUS_H
+#endif // HTTP_UTILS_H
