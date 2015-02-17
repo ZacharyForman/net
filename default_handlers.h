1c713ee3b649b999bf7ca68c20a24c64bf524235
ZacharyForman <au.zachary.forman@gmail.com>
2015-02-17 20:06:39 +1030

    Added definition of handler class.

--- default_handlers.h
+++ default_handlers.h
@@ -5,12 +5,21 @@
 #error "Requires C++11 features"
 #else
 
+#include <string>
+
 #include "handler.h"
 #include "http_status.h"
+#include "http_request.h"
 
 namespace net {
 
-
+class FileServer {
+public:
+  HttpStatus operator()(HttpRequest request) const;
+  explicit FileHandler(const ::std::string &root);
+private:
+  ::std::string root;
+};
 
 } // net
 
