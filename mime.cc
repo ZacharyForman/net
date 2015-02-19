08f5617e4c958155548722dfd026cdbb516cf3e3
= <=>
2015-02-19 21:30:34 +1030

    m

---
+++ mime.cc
@@ -0,0 +1,16 @@
+#if __cplusplus >= 201100L
+
+#include "mime.h"
+
+#include <string>
+
+namespace net {
+
+MimeType get_mime_type(::std::string extension)
+{
+
+}
+
+} // net
+
+#endif // __cplusplus >= 201100L
