40a3f2bebb8f61909be5a157661fbf6f00ef3aed
Zac <au.zachary.forman@gmail.com>
2015-03-02 23:45:54 +1030

    more comments

--- handler.h
+++ handler.h
@@ -14,11 +14,15 @@ namespace net {
 
 namespace internals {
 
+// Base class that all handler internals are derived from.
+// We do this for polymorphism on the operator().
 class BaseHandler {
 public:
   virtual HttpStatus operator()(const HttpRequest &req) = 0;
 };
 
+// Wraps anything that can be called as though it was a function
+// HttpStatus func(const HttpRequest &req);
 template<typename T>
 class DerivedHandler : public BaseHandler {
 public:
@@ -30,6 +34,7 @@ private:
   T h;
 };
 
+// Utility method to make type deduction easier
 template<typename T>
 BaseHandler *make_handler(T t) {
   return new DerivedHandler<T>(t);
@@ -37,6 +42,10 @@ BaseHandler *make_handler(T t) {
 
 } // internals
 
+// Wraps anything that can be called as though it was a function
+// HttpStatus func(const HttpRequest &req);
+// Contains a base handler, and has the same type regardless
+// of what it's constructed from.
 class Handler {
 public:
   template<typename T>
